a5446430535808a77eebb99748c24ef6aef1e72f
Demo User <demouser@Macintosh-31.local>
2014-07-31 15:16:22 +0100

    add launch icon

--- PoolUp/app/src/main/java/poolup/penguins/chocolate/com/poolup/Launcher.java
+++ PoolUp/app/src/main/java/poolup/penguins/chocolate/com/poolup/Launcher.java
@@ -1,6 +1,8 @@
 package poolup.penguins.chocolate.com.poolup;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.graphics.Picture;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +14,7 @@ public class Launcher extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        ObjectAnimator animator = new ObjectAnimator();
     }
 
 
